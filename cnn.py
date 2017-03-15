a951ca81924bfa7699653ceaf550003ae0da008e
William Zhang <will800228@gmail.com>
2017-03-15 00:41:01 -0400

    use a diff lib keras for cnn

--- cnn.py
+++ cnn.py
@@ -3,77 +3,95 @@ import os
 import cv2
 import scipy.io as sio
 import numpy as np
-import tflearn
-from tflearn.data_utils import shuffle
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
-from tflearn.data_preprocessing import ImagePreprocessing
-from tflearn.data_augmentation import ImageAugmentation
-from tflearn.layers.conv import highway_conv_2d, max_pool_2d, conv_2d
-from tflearn.layers.normalization import local_response_normalization, batch_normalization
-import pickle
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+batch_size = 128
+num_classes = 20
+epochs = 100
+
+# input image dimensions
+img_rows, img_cols = 90, 90
 
 # Load the data set
 os.chdir('/Users/William/Google Drive/UW/STAT441/data_challenge_2/')
 dataSet = sio.loadmat('FacesDataChallenge.mat')
 
 
-Ytrain = dataSet['Y_train'].reshape([-1, 1])
+Ytrain = dataSet['Y_train']
 
-Xtrain = []
+Xtrain = np.empty(shape=(425, 90, 90))
 os.chdir('/Users/William/Google Drive/UW/STAT441/data_challenge_2/transformed_train')
 for i in range(425):
     img = cv2.imread(str(i) + '.png', 0)
-    Xtrain.append(np.array(img, dtype = float))#cv2.IMREAD_GRAYSCALE))
+    Xtrain[i] = np.array(img, dtype = float)
 
-# Shuffle the data
-X, Y = shuffle(Xtrain, Ytrain)
-X = X.reshape([-1, 90, 90, 1])
-
-Y = Y.reshape([-1, ])
-# Make sure the data is normalized
-img_prep = ImagePreprocessing()
-img_prep.add_featurewise_zero_center()
-img_prep.add_featurewise_stdnorm()
-
-# Create extra synthetic training data by flipping, rotating and blurring the
-# images on our data set.
-img_aug = ImageAugmentation()
-img_aug.add_random_flip_leftright()
-img_aug.add_random_rotation(max_angle=25.)
-#img_aug.add_random_blur(sigma_max=3.)
-
-# Define our network architecture:
-network = input_data(shape=[None, 90, 90, 1])
-
-#                     data_preprocessing=img_prep,
-#                     data_augmentation=img_aug)
-
-network = conv_2d(network, 32, 3, activation='relu', regularizer="L2")
-network = max_pool_2d(network, 2)
-network = local_response_normalization(network)
-network = conv_2d(network, 64, 3, activation='relu', regularizer="L2")
-network = max_pool_2d(network, 2)
-network = local_response_normalization(network)
-network = fully_connected(network, 128, activation='tanh')
-network = dropout(network, 0.8)
-network = fully_connected(network, 256, activation='tanh')
-network = dropout(network, 0.8)
-network = fully_connected(network, 20, activation='softmax')
-network = regression(network, optimizer='adam', learning_rate=0.01,
-                     loss='categorical_crossentropy', name='target')
-
-model = tflearn.DNN(network, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=1, show_metric=True, run_id='convnet_highway_mnist')
-
+Xtest = np.empty(shape=(150, 90, 90))
 os.chdir('/Users/William/Google Drive/UW/STAT441/data_challenge_2/transformed_test')
-Xtest = np.empty([150, 90, 90])
 for i in range(150):
-    Xtest[i] = np.array(cv2.imread(str(i) + '.png', 0), dtype = float)
+    img = cv2.imread(str(i) + '.png', 0)
+    Xtest[i] = np.array(img, dtype = float)
 
-Xtest = Xtest.reshape([-1, 90, 90, 1])
+
+if K.image_data_format() == 'channels_first':
+    Xtrain = Xtrain.reshape(Xtrain.shape[0], 1, img_rows, img_cols)
+    Xtest = Xtest.reshape(Xtest.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    Xtrain = Xtrain.reshape(Xtrain.shape[0], img_rows, img_cols, 1)
+    Xtest = Xtest.reshape(Xtest.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+Xtrain = Xtrain.astype('float32')
+Xtest = Xtest.astype('float32')
+Xtrain /= 255
+Xtest /= 255
+print('x_train shape:', Xtrain.shape)
+print(Xtrain.shape[0], 'train samples')
+print(Xtest.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Ytrain = keras.utils.to_categorical(Ytrain - 1, num_classes)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(Xtrain, Ytrain, batch_size=batch_size, epochs=epochs,
+          verbose=1)
 
 cnn_result = model.predict(Xtest)
 
-np.savetxt("cnn_result.csv", np.dstack((np.arange(1, cnn_result.size+1),cnn_result))[0],"%d,%d", \
-           header="Id,ClassLabel", comments='')
+score = model.evaluate(Xtrain, Ytrain, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+os.chdir('/Users/William/Google Drive/UW/STAT441/data_challenge_2')
+
+with open('cnn_result.csv', 'w') as resultFile:
+    resultFile.write('Id,ClassLabel\n')
+    for i in range(150):
+        biggest = 0
+        p = 0
+        for j in range(20):
+            s = cnn_result[i, j]
+            if s > biggest:
+                biggest = s
+                p = j + 1
+        resultFile.write('%d,%d\n' % (i + 1, p))
